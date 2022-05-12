7aa1ab045845da3bb79cde31e211645f87193a5a
ann.Miin <annyloyz@hotmail.com>
2022-05-11 21:59:29 -0500

    algo

--- single.php
+++ single.php
@@ -2,18 +2,19 @@
 
 	<section class="container">
 
-		<img src="https://e.rpp-noticias.io/large/2022/05/04/152015_1254906.jpg?v=1651712849" class="img-fluid">
-		<center>
 		
 		<h1> <?php the_title(); ?> </h1>
-		</center>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+		<h5><i></a><?php the_field('resumen'); ?>
+		</i></h5>
+
+		<?php the_post_thumbnail("large"); ?>
+
+		<?php the_content(); ?>
+		<h4>Fuente:
+			<?php the_field('fuente'); ?>
+				
+		</h4>
 
 	</section>
 
