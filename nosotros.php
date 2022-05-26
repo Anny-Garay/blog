1364c7007a9e469d6effe8670c2a3883adb69b0e
ann.Miin <annyloyz@hotmail.com>
2022-05-25 21:59:31 -0500

    bien

---
+++ nosotros.php
@@ -0,0 +1,8 @@
+<?php /*Template Name: Nosotros */
+get_header(); ?>
+
+<div class="container">
+<h2>Aqui va mi plantilla</h2>
+</div>
+
+<?php get_footer(); ?>
---
+++ portada.php
@@ -0,0 +1,8 @@
+<?php /*Template Name: Portada */
+get_header(); ?>
+
+<div class="conteiner">
+<h2>Estoy en la portada</h2>
+</div>
+
+<?php get_footer(); ?>
